2c67b199c6d2218f77c478d9ca8495702953bed0
Chizhikov <57985484+SergioChizhikov@users.noreply.github.com>
2024-10-07 11:16:08 +0300

    Ходит не далее 5 страницы, вбивает по букве на главной

--- bot.js
+++ bot.js
@@ -15,18 +15,40 @@ let searchBtn = document.getElementsByName("btnK")[0];
 let keywords = ["вывод произвольных полей wordpress", "10 самых популярных шрифтов от Google", "Отключение редакций и ревизий в WordPress"];
 let keyword = keywords[getRandom(0, keywords.length)];
 
-
+//Работаем на главной странице поисковика
 if (searchBtn !== undefined) {
-  input.value = keyword;
-  searchBtn.click();
+  let i = 0;
+  let timerId = setInterval(function() {
+    input.value += keyword[i];
+    i++;
+    if (i == keyword.length) {
+      clearInterval(timerId);
+      searchBtn.click();
+    }
+  }, 300)
+//Работаем на странице поисковой выдачи  
 } else {
+  let nextPage = true;
   for (let i = 0; i < links.length; i++) {
     if (links[i].href.indexOf('napli.ru') != -1) {
       let link = links[i];
+      nextPage = false;
       console.log("Нашел строку " + link);
-      //link.click();
+      setTimeout(()=>{
+        link.click();
+      }, getRandom(2000, 4000)); 
       break;
     }
+  }
+  if (document.querySelector(".YyVfkd").innerText == "5") {
+    nextPage = false;
+    location.href = "https://www.google.com/";
+  }
+  if (nextPage) {
+    setTimeout(()=>{
+      pnnext.click();
+    }, getRandom(2500, 4000))
+ 
   }
 }
 
